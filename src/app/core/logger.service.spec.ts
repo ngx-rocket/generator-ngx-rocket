f93ae1222747a89f7868a001f3128110276125e1
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-01-04 11:16:23 +0100

    Added logger documentation and unit tests

--- src/app/core/logger.service.spec.ts
+++ src/app/core/logger.service.spec.ts
@@ -1,9 +1,71 @@
-import { Logger } from './logger.service';
+import { Logger, LogLevel } from './logger.service';
+
+const logMethods = ['log', 'info', 'warn', 'error'];
 
 describe('Logger', () => {
+  let savedConsole, savedLevel, savedOutputs;
+
+  beforeAll(() => {
+    savedConsole = [];
+    logMethods.forEach((m) => {
+      savedConsole[m] = console[m];
+      console[m] = () => {};
+    });
+    savedLevel = Logger.level;
+    savedOutputs = Logger.outputs;
+  });
+
+  afterAll(() => {
+    logMethods.forEach((m) => { console[m] = savedConsole[m]; });
+    Logger.level = savedLevel;
+    Logger.outputs = savedOutputs;
+  });
 
   it('should create an instance', () => {
     expect(new Logger()).toBeTruthy();
+  });
+
+  it('should add a new LogOutput and receives log entries', () => {
+    // Arrange
+    let outputSpy = jasmine.createSpy('outputSpy');
+    let log = new Logger('test');
+
+    // Act
+    Logger.outputs.push(outputSpy);
+
+    log.debug('d');
+    log.info('i');
+    log.warn('w');
+    log.error('e', { error: true });
+
+    // Assert
+    expect(outputSpy).toHaveBeenCalled();
+    expect(outputSpy.calls.count()).toBe(4);
+    expect(outputSpy).toHaveBeenCalledWith('test', LogLevel.Debug, 'd');
+    expect(outputSpy).toHaveBeenCalledWith('test', LogLevel.Info, 'i');
+    expect(outputSpy).toHaveBeenCalledWith('test', LogLevel.Warning, 'w');
+    expect(outputSpy).toHaveBeenCalledWith('test', LogLevel.Error, 'e', { error: true });
+  });
+
+  it('should add a new LogOutput and receives only production log entries', () => {
+    // Arrange
+    let outputSpy = jasmine.createSpy('outputSpy');
+    let log = new Logger('test');
+
+    // Act
+    Logger.outputs.push(outputSpy);
+    Logger.enableProductionMode();
+
+    log.debug('d');
+    log.info('i');
+    log.warn('w');
+    log.error('e', { error: true });
+
+    // Assert
+    expect(outputSpy).toHaveBeenCalled();
+    expect(outputSpy.calls.count()).toBe(2);
+    expect(outputSpy).toHaveBeenCalledWith('test', LogLevel.Warning, 'w');
+    expect(outputSpy).toHaveBeenCalledWith('test', LogLevel.Error, 'e', { error: true });
   });
 
 });
