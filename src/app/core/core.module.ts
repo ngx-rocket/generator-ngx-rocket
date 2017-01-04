f19f1a9e71402c130b48c6ec9d1fbacd5ba65e18
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-01-04 16:52:46 +0100

    Added proper routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HeaderComponent } from './header/header.component';
@@ -7,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     NgbModule.forRoot()
   ],
   exports: [
