0763e4002c1102617b5b77795e58ca5262b96ce2
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-01-16 13:42:15 +0100

    Added backend proxy example

---
+++ proxy.conf.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const HttpsProxyAgent = require('https-proxy-agent');
+
+/*
+ * API proxy configuration.
+ * This allows you to proxy HTTP request like `http.get('/api/stuff')` to another server/port.
+ * This is especially useful during app development to avoid CORS issues while running a local server.
+ * For more details and options, see https://github.com/angular/angular-cli#proxy-to-backend
+ */
+const proxyConfig = [
+  {
+    context: '/api',
+    pathRewrite: {'^/api': ''},
+    target: 'http://api.icndb.com',
+    changeOrigin: true
+  }
+];
+
+/*
+ * Configures a corporate proxy agent for the API proxy if needed.
+ */
+function setupForCorporateProxy(proxyConfig) {
+  if (!Array.isArray(proxyConfig)) {
+    proxyConfig = [proxyConfig];
+  }
+
+  const proxyServer = process.env.http_proxy || process.env.HTTP_PROXY;
+  let agent = null;
+
+  if (proxyServer) {
+    console.log(`Using corporate proxy server: ${proxyServer}`);
+    agent = new HttpsProxyAgent(proxyServer);
+    proxyConfig.forEach(entry => { entry.agent = agent; });
+  }
+
+  return proxyConfig;
+}
+
+module.exports = setupForCorporateProxy(proxyConfig);
