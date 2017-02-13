f6f44f9585a9d207a771f49d53b51ce16ced15b0
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-02-13 11:55:57 +0100

    Added extension comment

--- src/app/core/http/request-options-args.ts
+++ src/app/core/http/request-options-args.ts
@@ -1,5 +1,7 @@
 import { RequestOptionsArgs } from '@angular/http';
 
+// RequestOptionArgs is declared in a re-exported module, so we have to extend the original module to make it work
+// properly (see  https://github.com/Microsoft/TypeScript/issues/13897)
 declare module '@angular/http/src/interfaces' {
 
   // Use this interface to extend requests options if needed
