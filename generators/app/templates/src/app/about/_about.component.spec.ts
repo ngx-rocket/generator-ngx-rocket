5a8e2a576b32fad02baa5fdf3fbf0a095544f6a4
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-07-24 17:07:30 +0200

    Ionic about module

--- generators/app/templates/src/app/about/_about.component.spec.ts
+++ generators/app/templates/src/app/about/_about.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+<% if (props.ui === 'ionic') { -%>
+import { IonicModule } from 'ionic-angular';
+<% } -%>
 
 import { AboutComponent } from './about.component';
 
@@ -8,6 +11,9 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+<% if (props.ui === 'ionic') { -%>
+        imports: [IonicModule.forRoot(AboutComponent)],
+<% } -%>
         declarations: [AboutComponent]
       })
       .compileComponents();
--- generators/app/templates/src/app/about/_about.module.ts
+++ generators/app/templates/src/app/about/_about.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+<% if (props.ui === 'ionic') { -%>
+import { IonicModule } from 'ionic-angular';
+<% } -%>
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
@@ -9,8 +12,16 @@ import { AboutComponent } from './about.component';
   imports: [
     CommonModule,
     TranslateModule,
+<% if (props.ui === 'ionic') { -%>
+    IonicModule,
+<% } -%>
     AboutRoutingModule
   ],
+<% if (props.ui === 'ionic') { -%>
+  entryComponents: [
+    AboutComponent
+  ],
+<% } -%>
   declarations: [
     AboutComponent
   ]
