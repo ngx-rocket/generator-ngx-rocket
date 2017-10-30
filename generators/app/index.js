d6c22f6e7cb58ed07243b1d9c2ccd0e2121ddcbb
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2017-10-30 17:13:58 +0100

    Updated generator output and scripts for yarn

--- generators/app/index.js
+++ generators/app/index.js
@@ -15,7 +15,7 @@ class NgxGenerator extends Generator {
     this.insight = new Insight({trackingCode: 'UA-93069862-1', pkg});
 
     this.argument('appName', {
-      desc: 'Name of the app to generate',
+      description: 'Name of the app to generate',
       type: String,
       required: false
     });
@@ -78,8 +78,13 @@ class NgxGenerator extends Generator {
     const skipInstall = this.options['skip-install'];
 
     if (!skipInstall) {
-      this.log(`\nRunning ${chalk.yellow('npm install')}, please wait...`);
-      this.npmInstall(null, {loglevel: 'error'});
+      this.log(`\nRunning ${chalk.yellow(`${this.packageManager} install`)}, please wait...`);
+
+      if (this.packageManager === 'yarn') {
+        this.yarnInstall();
+      } else {
+        this.npmInstall(null, {loglevel: 'error'});
+      }
     }
   }
 
@@ -90,22 +95,22 @@ class NgxGenerator extends Generator {
     }
 
     this.log('\nAll done! Get started with these tasks:');
-    this.log(`- $ ${chalk.green('npm start')}: start dev server with live reload on http://localhost:4200`);
+    this.log(`- $ ${chalk.green(`${this.packageManager} run start`)}: start dev server with live reload on http://localhost:4200`);
 
     if (this.props.target.includes('web')) {
-      this.log(`- $ ${chalk.green('npm run build')}: build web app for production`);
+      this.log(`- $ ${chalk.green(`${this.packageManager} run build`)}: build web app for production`);
     }
 
     if (this.props.target.includes('cordova')) {
-      this.log(`- $ ${chalk.green('npm run cordova:prepare')}: prepare for building mobile app`);
-      this.log(`- $ ${chalk.green('npm run cordova:run')}: run app on device or simulator`);
-      this.log(`- $ ${chalk.green('npm run cordova:build')}: build mobile app for production`);
+      this.log(`- $ ${chalk.green(`${this.packageManager} run cordova:prepare`)}: prepare for building mobile app`);
+      this.log(`- $ ${chalk.green(`${this.packageManager} run cordova:run`)}: run app on device or simulator`);
+      this.log(`- $ ${chalk.green(`${this.packageManager} run cordova:build`)}: build mobile app for production`);
     }
 
-    this.log(`- $ ${chalk.green('npm test')}: run unit tests in watch mode for TDD`);
-    this.log(`- $ ${chalk.green('npm run test:ci')}: lint code and run units tests with coverage`);
-    this.log(`- $ ${chalk.green('npm run e2e')}: launch e2e tests`);
-    this.log(`- $ ${chalk.green('npm run docs')}: show docs and coding guides`);
+    this.log(`- $ ${chalk.green(`${this.packageManager} run test`)}: run unit tests in watch mode for TDD`);
+    this.log(`- $ ${chalk.green(`${this.packageManager} run test:ci`)}: lint code and run units tests with coverage`);
+    this.log(`- $ ${chalk.green(`${this.packageManager} run e2e`)}: launch e2e tests`);
+    this.log(`- $ ${chalk.green(`${this.packageManager} run docs`)}: show docs and coding guides`);
   }
 }
 
