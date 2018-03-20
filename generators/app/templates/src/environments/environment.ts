a09dfb41c8a7cf6f743c39282cd3a96dd95b726e
Riceball LEE <snowyu.lee@gmail.com>
2018-03-20 11:35:58 +0800

    * make the version is semver compatible

--- generators/app/templates/src/environments/environment.ts
+++ generators/app/templates/src/environments/environment.ts
@@ -8,7 +8,7 @@ import env from './.env';
 
 export const environment = {
   production: false,
-  version: env.npm_package_version + '(dev)',
+  version: env.npm_package_version + '-dev',
   serverUrl: '/api',
   defaultLanguage: 'en-US',
   supportedLanguages: [
