98cc7d0ff947907b5a275b118e60faa228a2fad7
Captain Caius <241342+captaincaius@users.noreply.github.com>
2018-11-20 16:01:02 +0200

    Updated ionic to beta 16 and migrated for tabs breaking changes

--- generators/app/templates/src/app/shell/__ionic-tabs.shell.component.ts
+++ generators/app/templates/src/app/shell/__ionic-tabs.shell.component.ts
@@ -1,32 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { Tab } from '@ionic/angular';
-import { filter } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SettingsComponent } from '@app/settings/settings.component';
 import { AboutComponent } from '@app/about/about.component';
 import { HomeComponent } from '@app/home/home.component';
+import { ActivatedRoute, Router, Event, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent implements OnInit, OnDestroy {
   tabs = [
     { component: HomeComponent, name: 'home', route: 'home', title: 'Home', icon: 'home' },
     { component: AboutComponent, name: 'about', route: 'about', title: 'About', icon: 'logo-angular' },
     { component: SettingsComponent, name: 'settings', route: 'settings', title: 'Settings', icon: 'cog' }
   ];
-  selectedTabIndex: number;
-  subscription: any;
+  selectedTabName: string;
+  private routeEventSubscription: Subscription;
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute) {
   }
   ngOnInit() {
     this.updateTab(this.activatedRoute);
+    // Update ionic selected tab based on angular route events
+    this.routeEventSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.updateTab(this.activatedRoute));
   }
-  onTabChange(selectedTabElm: Tab) {
-    const selectedTab = this.tabs.find(tab => tab.name === selectedTabElm.name);
-    this.router.navigate([selectedTab.route]);
+  ngOnDestroy() {
+    if (this.routeEventSubscription) {
+      this.routeEventSubscription.unsubscribe();
+    }
   }
   private updateTab(route: ActivatedRoute) {
     if (!route || !route.firstChild) {
@@ -40,7 +45,7 @@ export class ShellComponent implements OnInit {
         route = route.firstChild;
       }
       // Fixed #19420 end
-      this.selectedTabIndex = this.tabs.findIndex(tab => tab.route === route.routeConfig.path);
+      this.selectedTabName = this.tabs.find(tabElement => tabElement.route == route.routeConfig.path).name;
     }
   }
 }
