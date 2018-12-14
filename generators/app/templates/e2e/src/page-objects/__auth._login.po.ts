8bdb5c49da85ae8084debe89018d3181ded8c336
y.lasorsa <y.lasorsa@criteo.com>
2018-12-14 16:46:43 +0100

    Fix Ionic e2e tests

--- generators/app/templates/e2e/src/page-objects/__auth._login.po.ts
+++ generators/app/templates/e2e/src/page-objects/__auth._login.po.ts
@@ -7,8 +7,9 @@ import { browser, element, by } from 'protractor';
 
 export class LoginPage {
 <%   if (props.ui === 'ionic') { -%>
-  usernameField = element(by.css('ion-input[formControlName="username"]'));
-  passwordField = element(by.css('ion-input[formControlName="password"]'));
+  // Custom locator to find an element within shadow DOM (by.deepCss() doesn't work here)
+  usernameField = element(by.shadowCss('ion-input[formControlName="username"]', 'input'));
+  passwordField = element(by.shadowCss('ion-input[formControlName="password"]', 'input'));
   loginButton = element(by.css('ion-button[type="submit"]'));
 <%   } else { -%>
   usernameField = element(by.css('input[formControlName="username"]'));
