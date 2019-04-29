8fd1ddb52d9655f90beb27cd1b962a0fbab124d8
Alexandre Koelsch <alexandre.koelsch@mythalesgroup.io>
2019-04-29 16:30:14 +0200

    feat: 🎸 Add Jest as an option
    Propose to replace Karma/Jasmine by Jest to manage unit tests. This commit adds only the choice and impacted configuration files. Unit tests are not yet fixed
    
    ✅ Closes: #464

---
+++ generators/app/templates/src/__tools-jest.jest.config.js
@@ -0,0 +1,11 @@
+// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@env': '<rootDir>/src/environments/environment'
+  },
+  transformIgnorePatterns: ['node_modules/(?!(jest-test))']
+};
