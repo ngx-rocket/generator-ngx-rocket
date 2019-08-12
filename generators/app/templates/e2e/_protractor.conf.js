a038567ac855e9951c7351aeb898cc01a16b53b7
Yohan Lasorsa <yolasors@microsoft.com>
2019-08-12 16:52:55 +0200

    Migrate to Azure Pipelines CI

--- generators/app/templates/e2e/_protractor.conf.js
+++ generators/app/templates/e2e/_protractor.conf.js
@@ -12,7 +12,8 @@ exports.config = {
   capabilities: {
     browserName: process.env.PROTRACTOR_BROWSER || 'chrome',
     chromeOptions: {
-      args: ['lang=en-US'],
+      binary: process.env.PROTRACTOR_CHROME_BIN || undefined,
+      args: process.env.PROTRACTOR_CHROME_ARGS ? JSON.parse(process.env.PROTRACTOR_CHROME_ARGS) : ['lang=en-US'],
       prefs: {
         intl: { accept_languages: 'en-US' }
       }
