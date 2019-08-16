01fcc71dc8e78c5530bcd7debffcbf2ec26b8226
Yohan Lasorsa <yolasors@microsoft.com>
2019-08-16 21:46:32 +0200

    Reverted test script to fix MacOS tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,8 +5,8 @@
 
 set -e
 
-CWD=$(pwd)
-SCRIPT_FOLDER=$(dirname "${BASH_SOURCE[0]}")
+CWD=`pwd`
+SCRIPT_FOLDER=`dirname "${BASH_SOURCE[0]}"`
 TEST_FOLDER=$CWD/sample-app
 CACHE_FOLDER=$CWD/cache
 OUT_FOLDER=$CWD/dist
@@ -61,7 +61,7 @@ do
         npm run test:ci -- --no-progress
 
         # force usage of local chrome binary, in headless mode
-        PROTRACTOR_CHROME_BIN=$(node -p "require('puppeteer').executablePath()") \
+        PROTRACTOR_CHROME_BIN=`node -p "require('puppeteer').executablePath()"` \
         PROTRACTOR_CHROME_ARGS='["lang=en-US","--headless","--disable-gpu","--window-size=1024,768"]' \
         npm run e2e
 
@@ -75,7 +75,7 @@ do
 
             # copy apk
             mkdir -p $OUT_FOLDER
-            APK_FILE=$(echo $1 | sed -e 's/[^A-Za-z0-9._-]/-/g')
+            APK_FILE=`echo $1 | sed -e 's/[^A-Za-z0-9._-]/-/g'`
             cp dist/*.apk $OUT_FOLDER/$APK_FILE.apk
 
         fi
