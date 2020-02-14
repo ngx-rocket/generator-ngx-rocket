ce17ea8778c2e5e7e2455dc664475ff14da15add
sinedied <noda@free.fr>
2020-02-14 10:45:39 +0100

    chore: update dependencies and fix jest config

--- generators/app/templates/__tools-jest._jest.config.js
+++ generators/app/templates/__tools-jest._jest.config.js
@@ -8,6 +8,12 @@ module.exports = {
     '@app/(.*)': '<rootDir>/src/app/$1',
     '@env': '<rootDir>/src/environments/environment'
   },
+  globals: {
+    'ts-jest': {
+      allowSyntheticDefaultImports: true,
+      tsConfig: '<rootDir>/tsconfig.spec.json'
+    },
+  },
   // Do not ignore librairies such as ionic, ionic-native or bootstrap to transform them during unit testing.
 <% const excludedLibrairies = ['jest-test']
   if (props.target.includes('cordova')) { excludedLibrairies.push('@ionic-native'); }
