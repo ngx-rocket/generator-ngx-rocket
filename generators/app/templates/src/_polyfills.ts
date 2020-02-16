88d3d58e6166ef4056badafa13dd94a70984e191
sinedied <noda@free.fr>
2020-02-16 16:11:42 +0100

    feat: update bootstrap template for ng9

--- generators/app/templates/src/_polyfills.ts
+++ generators/app/templates/src/_polyfills.ts
@@ -57,7 +57,13 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+<% if (props.ui === 'bootstrap') { -%>
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 
+<% } -%>
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
