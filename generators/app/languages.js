37d56568756604402ba6b5f2f603ae7a8e0fcbd4
sinedied <noda@free.fr>
2020-03-26 10:31:13 +0100

    fix: languages listing on windows

--- generators/app/languages.js
+++ generators/app/languages.js
@@ -1,8 +1,9 @@
 const path = require('path');
-const globby = require('globby');
+const fs = require('fs');
 
 // Extract languages from files available in /templates/src/translations/
 module.exports = function() {
-  const files = globby.sync(path.join(__dirname, 'templates/src/translations/*.json'));
+  const translationsDir = path.join(__dirname, 'templates/src/translations');
+  const files = fs.readdirSync(translationsDir).filter(file => path.extname(file) === '.json');
   return files.map(file => path.basename(file).slice(2, 7));
 };
