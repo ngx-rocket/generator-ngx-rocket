840c033d1c2d4fa55faf71e9cb2ece3ed9a82f72
sinedied <noda@free.fr>
2020-09-01 14:57:58 +0200

    fix: remove unused modules (close #560)

--- generators/app/templates/src/app/home/_home.module.ts
+++ generators/app/templates/src/app/home/_home.module.ts
@@ -10,20 +10,17 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { Angulartics2Module } from 'angulartics2';
 <% } -%>
 
-import { CoreModule } from '@core';
 import { SharedModule } from '@shared';
 <% if (props.ui === 'material') { -%>
 import { MaterialModule } from '@app/material.module';
 <% } -%>
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { QuoteService } from './quote.service';
 
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule,
-    CoreModule,
     SharedModule,
 <% if (props.ui === 'ionic') { -%>
     IonicModule,
