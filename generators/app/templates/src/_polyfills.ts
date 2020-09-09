117df80d75b96e96290e31c338e487ddd4172584
sinedied <noda@free.fr>
2020-09-09 15:44:50 +0200

    chore: fix zone.js version to match Angular requirements

--- generators/app/templates/src/_polyfills.ts
+++ generators/app/templates/src/_polyfills.ts
@@ -4,6 +4,7 @@
  */
 import '@angular/localize/init';
 <% } -%>
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
