c91c2a3ddaebeabe1a36da9293b63ccfa364b2ca
Fabio Martino <martino.fabio@libero.it>
2020-12-06 19:04:16 +0100

    WIP ng11

--- generators/app/templates/src/_main.ts
+++ generators/app/templates/src/_main.ts
@@ -9,16 +9,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from '@app/app.module';
 import { environment } from '@env/environment';
-import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
-
-if (environment.hmr) {
-  hmrBootstrap(module, bootstrap);
-} else {
-  bootstrap().catch(err => console.error(err));
-}
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
