6397d966b941ef3c2d0995ae5b2a082df768f6a6
Fabio Martino <martino.fabio@libero.it>
2020-12-18 02:22:03 +0100

    Update to ng11

--- generators/app/templates/src/environments/_environment.prod.ts
+++ generators/app/templates/src/environments/_environment.prod.ts
@@ -8,7 +8,6 @@ import { env } from './.env';
 
 export const environment = {
   production: true,
-  hmr: false,
   version: env.npm_package_version,
   serverUrl: 'https://api.chucknorris.io',
   defaultLanguage: '<%= props.languages.includes('en-US') ? 'en-US' : props.languages[0] %>',
