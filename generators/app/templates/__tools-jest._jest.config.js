e42d20624d0a1b035a6dc059430b593a8d2ca2b1
Yash Anand <yashanand1910@gmail.com>
2021-09-20 18:21:09 +0530

    Update _package.json

--- generators/app/templates/__tools-jest._jest.config.js
+++ generators/app/templates/__tools-jest._jest.config.js
@@ -2,7 +2,7 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
-const { compilerOptions } = require('./tsconfig');
+const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
   preset: 'jest-preset-angular',
