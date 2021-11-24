43d323bdc2abfcce6760d2b35eae8a3d0faa55df
sinedied <noda@free.fr>
2021-11-24 10:32:41 +0100

    feat: update karma config

--- generators/app/templates/__tools-karma._karma.conf.js
+++ generators/app/templates/__tools-karma._karma.conf.js
@@ -14,10 +14,16 @@ module.exports = function(config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-junit-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       captureConsole: Boolean(process.env.KARMA_ENABLE_CONSOLE)
     },
@@ -27,10 +33,13 @@ module.exports = function(config) {
       useBrowserName: false,
       suite: '' // Will become the package name attribute in xml testsuite element
     },
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'text-summary'],
-      dir: path.join(__dirname, './reports/coverage'),
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/ng13'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
     reporters: ['progress', 'junit'],
     port: 9876,
