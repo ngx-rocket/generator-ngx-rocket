fa18d6f7023733fe652271e70b79828ce1bed3c0
sinedied <noda@free.fr>
2022-08-24 17:30:18 +0200

    feat: migrate to Angular 14 and upgrade dependencies
    BREAKING CHANGE:
    
    - Migrate to Angular 14
    - Requires Node.js 14+

--- generators/app/templates/__tools-jest._jest.config.js
+++ generators/app/templates/__tools-jest._jest.config.js
@@ -1,5 +1,4 @@
-require('jest-preset-angular/ngcc-jest-processor');
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require('./tsconfig.json');
