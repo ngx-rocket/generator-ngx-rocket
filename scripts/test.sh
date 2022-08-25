050c387d033666ea8e76ef290b27ac4272d95013
sinedied <noda@free.fr>
2022-08-25 11:50:38 +0200

    ci: fix e2e tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -61,22 +61,21 @@ do
 
         USE_PROTRACTOR=$(npm list --depth 0 --parseable protractor)
 
-        if [ -z "$USE_PROTRACTOR" ]; then
+        if [ ! -z "$USE_PROTRACTOR" ]; then
             echo "Setting up puppeteer for protractor..."
 
             # force specific puppeteer/webdriver version to match up
             if [ "$NGX_PACKAGE_MANAGER" == "yarn" ]; then
-                # yarn add npm i puppeteer@16.2.0
+                yarn add -D puppeteer@16.2.0
                 yarn run webdriver-manager update --versions.chrome 104.0.5112.79 --gecko=false
             else
-                # npm i -D puppeteer@16.2.0
+                npm i -D puppeteer@16.2.0
                 npx webdriver-manager update --versions.chrome 104.0.5112.79 --gecko=false
             fi
-
             # force usage of local chrome binary, in headless mode
             PROTRACTOR_CHROME_BIN=$(node -p "require('puppeteer').executablePath()") \
             PROTRACTOR_CHROME_ARGS='["lang=en-US","--headless","--disable-gpu","--window-size=1024,768"]' \
-            npm run e2e --if-present -- # --webdriver-update=false
+            npm run e2e --if-present
         else
             npm run e2e --if-present
         fi
